6f1c0be0b04e0a474f01527ef8e05043d722afd0
Deepak Vishwakarma <vishwakarmadeepak310@gmail.com>
2026-09-04 01:54:51 +0530

    feat: add src/main/java/dev/carbonledger/CarbonLedgerApplication.java

---
+++ src/main/java/dev/carbonledger/CarbonLedgerApplication.java
@@ -0,0 +1,11 @@
+package dev.carbonledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonLedgerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonLedgerApplication.class, args);
+    }
+}
